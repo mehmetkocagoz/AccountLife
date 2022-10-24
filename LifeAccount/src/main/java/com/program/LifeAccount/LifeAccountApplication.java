854cff3938208d4920c8f1a9d2729ade77d50bdf
Mehmet Kocagöz <103457586+mehmetkocagoz@users.noreply.github.com>
2022-10-24 19:46:50 +0300

    Add files via upload

---
+++ LifeAccount/src/main/java/com/program/LifeAccount/LifeAccountApplication.java
@@ -0,0 +1,13 @@
+package com.program.LifeAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeAccountApplication.class, args);
+	}
+
+}
---
+++ LifeAccount/src/test/java/com/program/LifeAccount/LifeAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.LifeAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
