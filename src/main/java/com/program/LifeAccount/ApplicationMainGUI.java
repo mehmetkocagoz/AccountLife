b17da2d36921d8d1f52f6c5d85b32c0e347f632d
Mehmet Kocagöz <mehmetkocagz@outlook.com>
2022-10-29 17:15:48 +0300

    I decided to use Vaadin for GUI

---
+++ src/main/java/com/program/LifeAccount/ApplicationMainGUI.java
@@ -0,0 +1,16 @@
+package com.program.LifeAccount;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+
+
+@PageTitle("LifeAccount")
+@Route(value = "LifeAccount")
+public class ApplicationMainGUI extends VerticalLayout {
+    public ApplicationMainGUI(){
+
+    }
+}
